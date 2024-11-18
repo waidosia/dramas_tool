2fd5133d1e345cc6f3568ae871e7bdd73b8fbaa5
caozh <2578052882@qq.com>
2024-11-18 22:35:36 +0800

    init

--- utils/logs.py
+++ utils/logs.py
@@ -7,14 +7,16 @@ logger = logging.getLogger()
 # 获取当前绝对路径
 def get_cwd():
     # 获取当前项目的根路径
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)),"../logs")
+    return os.path.join(os.getcwd(),"logs")
 
 def log_config():
     # 设置日志的的登记
     logging.basicConfig(level=logging.DEBUG)
 
     # 日志输出目录
+    os.makedirs(get_cwd(), exist_ok=True)
     log_path = os.path.join(get_cwd(),'flask.log')
+
     # 创建日志记录器，设置日志的保存路径和每个日志的大小和日志的总大小
     file_log_handler = RotatingFileHandler(log_path, encoding='UTF-8', maxBytes=1024*1024*100,backupCount=100)
     # 创建日志记录格式，日志等级，输出日志的文件名 行数 日志信息
